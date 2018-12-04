5eebd62cfe64d3374595e3487b04c67567112503
KYLE\Surface <kyle.public10415@gmail.com>
2018-12-04 10:40:18 +0800

    feat : add clear input field

--- src/components/InputPart.js
+++ src/components/InputPart.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react'
 import TodoPart from './TodoPart.js'
 
+
 export default class InputPart extends Component {
     state = {item : '', numberArray : [], size : 0}
 
     updateInput = (evt)=>{
         this.setState({item : evt.target.value})
     }
-    addItem = (evt)=>{    
+    addItem = (evt)=>{
+        
         this.setState(prevState => ({
             numberArray: [...prevState.numberArray, this.state.item]
           }))
+        this.setState({item : ''})
+        evt.preventDefault()
+        
     }
     render() {
         return (
         <div>
-            <input type="type" onChange={this.updateInput}></input>
+            <input value ={this.state.item} type="text" onChange={this.updateInput}></input>
             <button onClick={this.addItem}>todo!</button>
-            {this.state.numberArray.map(e=>(<TodoPart item={e}/>))}          
+            {this.state.numberArray.map(e=>(<TodoPart item={e}/>))}    
+                  
         </div>
         )
     }
